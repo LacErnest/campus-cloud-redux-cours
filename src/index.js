f2ba7522952e152aec9babd6d8a3b19d02fd3a16
Laceinstein <laceinstein@gmail.com>
2022-08-30 11:17:39 +0200

    add index.js content

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+// ------ HTML references ------
+let notesUList = document.getElementById('notes');
+let addNoteForm = document.getElementById('add-note');
+let addNoteTitle = addNoteForm['title'];
+let addNoteContent = addNoteForm['content'];
+
+// ------ Redux ------
+function deleteNote(index) {
+  // console.log(index);
+}
+
+function renderNotes() {
+  setDeleteNoteButtonsEventListeners();
+}
+
+// ------ Event Listeners ------
+addNoteForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // console.log('Title:', addNoteTitle.value, 'Content:', addNoteContent.value);
+});
+
+function setDeleteNoteButtonsEventListeners() {
+  let buttons = document.querySelectorAll('ul#notes li button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', () => {
+      deleteNote(button.dataset.id);
+    });
+  }
+}
+
+// ------ Render the initial Notes ------
+renderNotes();
