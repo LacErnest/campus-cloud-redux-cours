64e655a15329b9cd5eee94910ed9a5e572e0217f
Laceinstein <laceinstein@gmail.com>
2022-08-30 14:21:40 +0200

    root reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,28 +1,10 @@
-import { ADD_NOTE, REMOVE_NOTE} from '../actions/actions';
+import notesReducer from './notesReducers';
+import visibilityFilter from './visibilityFilter';
+import { combineReducers } from 'redux';
 
-const initialState = {
-  notes: [],
-};
+const reducers = combineReducers({
+  notes: notesReducer,
+  visibility: visibilityFilter,
+});
 
-function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_NOTE:
-      return {
-        notes: [
-          ...state.notes,
-          {
-            title: action.title,
-            content: action.content,
-          },
-        ],
-      };
-    case REMOVE_NOTE:
-      return {
-        notes: state.notes.filter((note, index) => index != action.id)
-      };
-    default:
-      return state;
-  }
-}
-
-export default rootReducer;
+export default reducers;
