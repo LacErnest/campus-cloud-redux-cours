288f25df62f132517ecb95230a92b68bfee86f6d
Laceinstein <laceinstein@gmail.com>
2022-08-30 12:19:01 +0200

    ajout de la suppression

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,7 +5,6 @@ export function addNote(title, content) {
   return { type: ADD_NOTE, title: title, content: content };
 }
 
-
 export function removeNote(id) {
   return { type: REMOVE_NOTE, id: id };
 }
